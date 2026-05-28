d624b7d74f96661d099bc229ac69208a1b93a472
ICthss <cfd.zhang3027@gmail.com>
2026-05-28 18:13:32 +0800

    添加了巧乐兹的种类

--- src/main/java/com/icthss/smasher/item/ModItems.java
+++ src/main/java/com/icthss/smasher/item/ModItems.java
@@ -22,8 +22,50 @@ public class ModItems {
     public static final DeferredItem<Item> AIRPODS = 
         ITEMS.register("airpods", () -> new Item(new Item.Properties()));
 
+    public static final DeferredItem<Item> QIAOLEZI = 
+        ITEMS.register("qiaolezi", () -> new Item(new Item.Properties()));
 
-     public static void register(IEventBus eventBus) {
+    public static final DeferredItem<Item> MACBOOK = 
+        ITEMS.register("macbook", () -> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> QIAOLEZI_DEEPEN =
+        ITEMS.register("qiaolezi_deepen", () -> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> QIAOLEZI_VANILLA =
+        ITEMS.register("qiaolezi_vanilla", () -> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> QIAOLEZI_BLUEBERRY =
+        ITEMS.register("qiaolezi_blueberry", () -> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> QIAOLEZISTRAWBERRY =
+        ITEMS.register("qiaolezi_strawberry", () -> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> QIAOLEZI_MATCHA =
+        ITEMS.register("qiaolezi_matcha", () -> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> QIAOLEZI_ENERGY =
+        ITEMS.register("qiaolezi_energy", () -> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> QIAOLEZI_SPRITE =
+        ITEMS.register("qiaolezi_sprite", () -> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> QIAOLEZI_FATAL = 
+        ITEMS.register("qiaolezi_fatal", () -> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> SPRITE = 
+        ITEMS.register("sprite", () -> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> SPRITE_BOTTLE = 
+        ITEMS.register("sprite_bottle", () -> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> FAKE_SPRITE = 
+        ITEMS.register("fake_sprite", () -> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> FAKE_SPRITE_BOTTLE = 
+        ITEMS.register("fake_sprite_bottle", () -> new Item(new Item.Properties()));
+        
+
+    public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
 }
