269cd4d1f0ef4d13aef5cec5636f17c16548a48f
ICthss <cfd.zhang3027@gmail.com>
2026-05-29 08:20:47 +0800

    修改了ModHarmfulFoodItem方法和注册参数

--- src/main/java/com/icthss/smasher/item/ModItems.java
+++ src/main/java/com/icthss/smasher/item/ModItems.java
@@ -2,7 +2,10 @@ package com.icthss.smasher.item;
 
 import com.icthss.smasher.Smasher;
 import com.icthss.smasher.item.ModFuelItems;
+import com.icthss.smasher.item.ModHarmfulFoodItem;
+import com.icthss.smasher.item.ModFoods;
 
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
@@ -18,34 +21,34 @@ public class ModItems {
         ITEMS.register("hollow_wood_coal", () -> new ModFuelItems(new Item.Properties(),3200));
 
     public static final DeferredItem<Item> QIAOLEZI_ORIGIN = 
-        ITEMS.register("qiaolezi_origin", () -> new ModHarmfulFoodItem(new Item.Properties().food(ModFoods.QIAOLEZI_ORIGIN)));
+        ITEMS.register("qiaolezi_origin", () -> new ModHarmfulFoodItem(new Item.Properties().food(ModFoods.QIAOLEZI_ORIGIN),ResourceLocation.fromNamespaceAndPath(Smasher.MODID, "qiaolezi_origin_penalty"), 2.0D));
 
     public static final DeferredItem<Item> QIAOLEZI = 
         ITEMS.register("qiaolezi", () -> new Item(new Item.Properties()));
 
     public static final DeferredItem<Item> QIAOLEZI_DEEPEN =
-        ITEMS.register("qiaolezi_deepen", () -> new ModHarmfulFoodItem(new Item.Properties().food(ModFoods.QIAOLEZI_DEEPEN)));
+        ITEMS.register("qiaolezi_deepen", () -> new ModHarmfulFoodItem(new Item.Properties().food(ModFoods.QIAOLEZI_DEEPEN),ResourceLocation.fromNamespaceAndPath(Smasher.MODID, "qiaolezi_deepen_penalty"), 2.0D ));
 
     public static final DeferredItem<Item> QIAOLEZI_VANILLA =
-        ITEMS.register("qiaolezi_vanilla", () -> new ModHarmfulFoodItem(new Item.Properties().food(ModFoods.QIAOLEZI_VANILLA)));
+        ITEMS.register("qiaolezi_vanilla", () -> new ModHarmfulFoodItem(new Item.Properties().food(ModFoods.QIAOLEZI_VANILLA),ResourceLocation.fromNamespaceAndPath(Smasher.MODID, "qiaolezi_vanilla_penalty"), 2.0D));
 
     public static final DeferredItem<Item> QIAOLEZI_BLUEBERRY =
-        ITEMS.register("qiaolezi_blueberry", () -> new ModHarmfulFoodItem(new Item.Properties().food(ModFoods.QIAOLEZI_BLUEBERRY)));
+        ITEMS.register("qiaolezi_blueberry", () -> new ModHarmfulFoodItem(new Item.Properties().food(ModFoods.QIAOLEZI_BLUEBERRY),ResourceLocation.fromNamespaceAndPath(Smasher.MODID, "qiaolezi_blueberry_penalty"), 2.0D));
 
     public static final DeferredItem<Item> QIAOLEZI_STRAWBERRY =
-        ITEMS.register("qiaolezi_strawberry", () -> new ModHarmfulFoodItem(new Item.Properties().food(ModFoods.QIAOLEZI_STRAWBERRY)));
+        ITEMS.register("qiaolezi_strawberry", () -> new ModHarmfulFoodItem(new Item.Properties().food(ModFoods.QIAOLEZI_STRAWBERRY),ResourceLocation.fromNamespaceAndPath(Smasher.MODID, "qiaolezi_strawberry_penalty"), 2.0D));
 
     public static final DeferredItem<Item> QIAOLEZI_MATCHA =
-        ITEMS.register("qiaolezi_matcha", () -> new ModHarmfulFoodItem(new Item.Properties().food(ModFoods.QIAOLEZI_MATCHA)));
+        ITEMS.register("qiaolezi_matcha", () -> new ModHarmfulFoodItem(new Item.Properties().food(ModFoods.QIAOLEZI_MATCHA),ResourceLocation.fromNamespaceAndPath(Smasher.MODID, "qiaolezi_matcha_penalty"), 2.0D));
 
     public static final DeferredItem<Item> QIAOLEZI_ENERGY =
-        ITEMS.register("qiaolezi_energy", () -> new ModHarmfulFoodItem(new Item.Properties().food(ModFoods.QIAOLEZI_ENERGY)));
+        ITEMS.register("qiaolezi_energy", () -> new ModHarmfulFoodItem(new Item.Properties().food(ModFoods.QIAOLEZI_ENERGY),ResourceLocation.fromNamespaceAndPath(Smasher.MODID, "qiaolezi_energy_penalty"), 2.0D));
 
     public static final DeferredItem<Item> QIAOLEZI_SPRITE =
-        ITEMS.register("qiaolezi_sprite", () -> new ModHarmfulFoodItem(new Item.Properties().food(ModFoods.QIAOLEZI_SPRITE)));
+        ITEMS.register("qiaolezi_sprite", () -> new ModHarmfulFoodItem(new Item.Properties().food(ModFoods.QIAOLEZI_SPRITE),ResourceLocation.fromNamespaceAndPath(Smasher.MODID, "qiaolezi_sprite_penalty"), 2.0D));
 
     public static final DeferredItem<Item> QIAOLEZI_FATAL = 
-        ITEMS.register("qiaolezi_fatal", () -> new ModHarmfulFoodItem(new Item.Properties().food(ModFoods.QIAOLEZI_FATAL)));
+        ITEMS.register("qiaolezi_fatal", () -> new ModHarmfulFoodItem(new Item.Properties().food(ModFoods.QIAOLEZI_FATAL),ResourceLocation.fromNamespaceAndPath(Smasher.MODID, "qiaolezi_fatal_penalty"), 2.0D));
 
     public static final DeferredItem<Item> SPRITE = 
         ITEMS.register("sprite", () -> new Item(new Item.Properties().food(ModFoods.SPRITE)));
