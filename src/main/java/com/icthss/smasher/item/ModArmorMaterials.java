90c3568a2f6d2fc49172e3e19a8d4c38c61c6a11
Edward <edward20090711@gmail.com>
2026-05-29 10:56:35 +0800

    修复了雪王盔甲部分代码的bug

--- src/main/java/com/icthss/smasher/item/ModArmorMaterials.java
+++ src/main/java/com/icthss/smasher/item/ModArmorMaterials.java
@@ -3,9 +3,10 @@ package com.icthss.smasher.item;
 import com.icthss.smasher.Smasher;
 import net.minecraft.Util;
 import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.sounds.SoundEvents; // 修复了这里的复数 s
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.crafting.Ingredient;
@@ -14,9 +15,10 @@ import java.util.EnumMap;
 import java.util.List;
 
 public class ModArmorMaterials {
-   
-    public static final Holder<ArmorMaterial> XUEWANG = BuiltInRegistries.ARMOR_MATERIAL.register(
-            "xuewang", 
+    
+    public static final Holder<ArmorMaterial> XUEWANG = Registry.registerForHolder(
+            BuiltInRegistries.ARMOR_MATERIAL,
+            ResourceLocation.fromNamespaceAndPath(Smasher.MODID, "xuewang"),
             new ArmorMaterial(
                     Util.make(new EnumMap<>(ArmorItem.Type.class), map -> {
                         map.put(ArmorItem.Type.BOOTS, 10);
@@ -24,15 +26,14 @@ public class ModArmorMaterials {
                         map.put(ArmorItem.Type.CHESTPLATE, 20);
                         map.put(ArmorItem.Type.HELMET, 20);
                     }),
-                    15, 
-                    SoundEvents.ARMOR_EQUIP_IRON, 
+                    15, // 附魔亲和性
+                    SoundEvents.ARMOR_EQUIP_IRON, // 装备音效
                     () -> Ingredient.EMPTY, // 修复材料
-                    
                     List.of(new ArmorMaterial.Layer(
                             ResourceLocation.fromNamespaceAndPath(Smasher.MODID, "xuewang")
                     )),
-                    1.0F,
-                    1.0F  
+                    1.0F, // 韧性
+                    1.0F  // 击退抗性
             )
     );
 }
