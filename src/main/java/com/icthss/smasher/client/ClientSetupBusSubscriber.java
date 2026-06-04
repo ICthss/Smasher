07fd87983ab23ff0815237ba14bd21d641b28a7f
ICthss <cfd.zhang3027@gmail.com>
2026-06-04 11:50:04 +0800

    删除了Blender相关内容

--- src/main/java/com/icthss/smasher/client/ClientSetupBusSubscriber.java
+++ src/main/java/com/icthss/smasher/client/ClientSetupBusSubscriber.java
@@ -3,7 +3,6 @@ package com.icthss.smasher.client;
 import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
 import com.icthss.smasher.gui.ModMenuTypes;
 import com.icthss.smasher.gui.SmasherScreen;
-import com.icthss.smasher.gui.BlenderScreen;
 
 public class ClientSetupBusSubscriber {
 
@@ -13,10 +12,6 @@ public class ClientSetupBusSubscriber {
         event.register(
                 ModMenuTypes.SMASHER_MENU.get(), 
                 SmasherScreen::new
-        );
-        event.register(
-                ModMenuTypes.BLENDER_MENU.get(),
-                BlenderScreen::new     
         );
         
     }
